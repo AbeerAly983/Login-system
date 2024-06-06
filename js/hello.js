7fe8aa6b8805a1ac4f084eb31847e98ef2d8fb21
Abeer Aly <abeerali25678@gmail.com>
2024-06-06 23:11:30 +0300

    edit-login

--- js/hello.js
+++ js/hello.js
@@ -4,7 +4,7 @@ console.log(cat);
 
 function display() {
   let cartona = "";
-  cartona += `<div class='d-flex align-items-center justify-content-center'>
+  cartona += `<div class='d-flex align-items-center flex-wrap justify-content-center'>
   <img src="imgs/undraw_stars_re_6je7__10_-removebg-preview.png"  width="100" alt="">
 
   <p class="navbar-brand pt-3" href="#">${cat}</p></div>
